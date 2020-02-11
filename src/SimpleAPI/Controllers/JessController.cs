bae04c154f65e11450de31f09bd8e068fc336e37
Jessie Furigay <jessf@peterpal.com.au>
2020-02-11 13:27:18 +0800

    update4

--- src/SimpleAPI/Controllers/JessController.cs
+++ src/SimpleAPI/Controllers/JessController.cs
@@ -13,7 +13,7 @@ namespace SimpleAPI.Controllers
     {
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Jessie", "Furigay" };
         }
 
     }
