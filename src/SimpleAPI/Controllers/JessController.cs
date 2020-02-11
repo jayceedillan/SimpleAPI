4515e3e3d8574013802af7bc817cf9386ac41da3
Jessie Furigay <jessf@peterpal.com.au>
2020-02-11 13:21:19 +0800

    update3

---
+++ src/SimpleAPI/Controllers/JessController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/jess")]
+    public class JessController : ControllerBase
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
